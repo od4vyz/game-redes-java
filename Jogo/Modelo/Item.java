5dc7f29179a9156dd058946cd6670778f7685e9b
Alisson Silva <alisson.santossilva7@gmail.com>
2025-10-18 01:39:43 -0300

    A base do jogo
    O esqueleto do jogo, o basico.

---
+++ Jogo/Modelo/Item.java
@@ -0,0 +1,20 @@
+package Jogo.Modelo;
+
+public class Item {
+    String nome;
+    String descricao;
+
+    public Item(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
---
+++ Jogo/Modelo/Jogador.java
@@ -0,0 +1,5 @@
+package Jogo.Modelo;
+
+public class Jogador {
+
+}
