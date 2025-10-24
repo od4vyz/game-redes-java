63a5e46457d08a4e7dc3100c4413ba7156ce5f68
Davi Oliveira <davi0209lima@gmail.com>
2025-10-24 00:12:13 -0300

    leitura de protocolo do lado cliente

--- src/cliente/LeitorCliente.java
+++ src/cliente/LeitorCliente.java
@@ -16,10 +16,42 @@ public class LeitorCliente implements Runnable{
             String mensagem;
 
             while((mensagem = entrada.readLine()) != null){
-                System.out.println(mensagem);
-            }
+
+                System.out.println(processarMensagem(mensagem));
+
+            }   
         }catch(Exception e){
             System.out.println("Erro de conexao com o servidor. " + e.getMessage());
         }
+    }
+
+    private String processarMensagem(String mensagemBruta){
+
+        //recebe a mensagem na formatacao de protocolo e quebra para interpretacao
+        String [] parametros = mensagemBruta.split("\\|");
+        
+        String comando = parametros[0]; 
+
+        String mensagem;
+
+        switch (comando) {
+            case "DESCRICAO":
+            case "NARRACAO":
+                mensagem = parametros[1];
+                break;
+
+            case "CHAT":
+                mensagem = "[" + parametros[1] + "]" + " " + parametros[2];
+                break;
+
+            case "ERRO":
+                mensagem = "[ERRO] " + parametros[1];
+                break;
+        
+            default:
+                mensagem = "[SERVIDOR] Não foi possível processar a mensagem.";
+                break;
+        }
+        return mensagem;
     }
 }
